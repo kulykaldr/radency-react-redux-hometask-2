17990d645d0e0695bf287c4c7bc8ef216bcaf711
Kulyk Aleksandr <16745608+kulykaldr@users.noreply.github.com>
2022-04-10 13:04:43 +0300

    update dependencies

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Container: React.FC = ({ children }) => {
+const Container: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className='container'>
       {children}
